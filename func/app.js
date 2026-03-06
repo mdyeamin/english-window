57f741ca6e2be481e72d0116e1a7ce1cd14213c6
mdyeamin <ahmedyeamin05@gmail.com>
2026-03-06 15:34:52 +0600

    show display word by clicking lesson button dnamically

--- func/app.js
+++ func/app.js
@@ -5,27 +5,66 @@ const lessonData = async () => {
 
   const data = await res.json();
 
-  console.log(showLevels(data.data));
+  showLevels(data.data);
 };
 lessonData();
 const showLevels = (levels) => {
   const container = document.getElementById("lessons");
 
   levels.forEach((level) => {
-    console.log(level);
-
     const button = document.createElement("button");
     button.innerHTML = `
         <button onclick="loadLevel(${level.level_no})" class="btn btn-outline btn-primary">
         <i class="fa-brands fa-leanpub"></i>
         Lesson - ${level.level_no}</button>`;
 
-        container.append(button)
+    container.append(button);
   });
 };
 
-const loadLevel=(id)=>{
-    console.log(id);
-    
+const loadLevel = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
 
-}
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showWordOnUi(data.data));
+};
+
+const showWordOnUi = async (words) => {
+  const currentData = document.getElementById("currentData");
+  currentData.innerHTML = "";
+  words.forEach((word) => {
+      // console.log(word);
+      const wordDiv = document.createElement("div");
+
+    wordDiv.innerHTML = `
+            <div class="bg-white rounded-2xl p-5 h-72 text-center shadow-sm">
+
+                <!-- Word -->
+                <h2 class="text-3xl font-bold mb-2">${word.word}</h2>
+
+                <!-- Subtitle -->
+                <p class="text-gray-600 mb-4">Meaning /Pronounciation</p>
+
+                <!-- Meaning -->
+                <p class="text-2xl text-gray-700 font-medium mb-10">
+                    "${word.meaning} / ${word.pronunciation}"
+                </p>
+
+                <!-- Buttons -->
+                <div class="flex justify-between mb-0">
+
+                    <!-- Info Button -->
+                    <button class="bg-gray-200 p-3 rounded-lg hover:bg-gray-300 transition">
+                        <span class="text-lg">ℹ️</span>
+                    </button>
+
+                    <!-- Sound Button -->
+                    <button class="bg-gray-200 p-3 rounded-lg hover:bg-gray-300 transition">
+                        <span class="text-lg">🔊</span>
+                    </button>
+                </div>
+        </div>`;
+        currentData.append(wordDiv);
+  });
+};
